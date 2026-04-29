7da5b2f3c75ae025655c390b5d933e5340e975b9
Jayjeet1013 <jayjeet1013@gmail.com>
2026-04-29 22:30:11 +0530

    feat: update OgStorage

---
+++ lib/services/OgStorage.ts
@@ -0,0 +1,34 @@
+import { Indexer, MemData } from "@0gfoundation/0g-ts-sdk";
+import { ethers } from "ethers";
+
+
+const RPC_URL = "https://evmrpc-testnet.0g.ai";
+const INDEXER_RPC = "https://indexer-storage-testnet-turbo.0g.ai";
+
+const provider = new ethers.JsonRpcProvider(RPC_URL);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+
+const indexer = new Indexer(INDEXER_RPC);
+
+// 🔥 SAVE DATA TO 0G
+export async function savePortfolio(data: any) {
+  try {
+    const encoded = new TextEncoder().encode(JSON.stringify(data));
+
+    const memData = new MemData(encoded);
+
+    await memData.merkleTree();
+
+    const [tx, err] = await indexer.upload(memData, RPC_URL, signer);
+
+    if (err) throw new Error(err);
+
+    return {
+      success: true,
+      rootHash: tx?.rootHash || "unknown",
+    };
+  } catch (error) {
+    console.error("0G Storage Error:", error);
+    return { success: false };
+  }
+}
