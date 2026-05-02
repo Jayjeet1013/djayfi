894e8f89674b3b7b73add4ae81d10773eed7c163
Jayjeet1013 <jayjeet1013@gmail.com>
2026-05-02 13:35:17 +0530

    fix : solve bug

--- lib/services/OgStorage.ts
+++ lib/services/OgStorage.ts
@@ -1,7 +1,6 @@
 import { Indexer, MemData } from "@0gfoundation/0g-ts-sdk";
 import { ethers } from "ethers";
 
-
 const RPC_URL = "https://evmrpc-testnet.0g.ai";
 const INDEXER_RPC = "https://indexer-storage-testnet-turbo.0g.ai";
 
